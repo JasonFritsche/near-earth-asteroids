d4eebe9a7658371e89bf0f8fbd4c18f8d39b8b6d
jasonfritsche <fritsche.jason@gmail.com>
2021-11-13 16:34:45 -0600

    feature: emit search form values to home component. updated  getAsteroids service func

--- src/services/Neows.service.js
+++ src/services/Neows.service.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const api_key = process.env.VUE_APP_NEOWS_API_KEY;
 
-export async function getAsteroids(fromDate, toDate) {
+export async function getAsteroidsResgetAsteroids(fromDate, toDate) {
   const response = await axios.get(
     `https://api.nasa.gov/neo/rest/v1/feed?start_date=${fromDate}&end_date=${toDate}&api_key=${api_key}`
   );
