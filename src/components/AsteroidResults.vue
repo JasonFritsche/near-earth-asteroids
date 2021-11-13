fb8f881e4a160bb266c11572dd0ba5a62aac70d9
jasonfritsche <fritsche.jason@gmail.com>
2021-11-13 17:32:17 -0600

    list asteroids by date

---
+++ src/components/AsteroidResults.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="asteroid-results-container">
+    <v-card class="mx-auto" max-width="400" tile>
+      <v-list-item
+        two-line
+        v-for="(item, date) in nearEarthObjects"
+        :key="date"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ date }}</v-list-item-title>
+          <v-list-item-subtitle v-for="(asteroidItem, i) in item" :key="i">{{
+            asteroidItem.name
+          }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["asteroidData"],
+  data: () => ({
+    nearEarthObjects: null,
+  }),
+  mounted() {
+    this.parseAsteroidData();
+  },
+  methods: {
+    parseAsteroidData() {
+      console.log(this.asteroidData);
+      this.nearEarthObjects = this.asteroidData.near_earth_objects;
+    },
+    getDate(obj) {
+      console.log(obj);
+    },
+  },
+};
+</script>
+
+<style>
+.v-expansion-panel {
+  max-width: 500px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,33 @@
 <template>
   <v-container>
     <v-row> Near Earth Asteroids </v-row>
-    <v-row><AsteroidSearchForm @search="search($event)" /></v-row>
+    <v-row>
+      <AsteroidSearchForm @search="search($event)" />
+    </v-row>
+    <v-row v-if="asteroidData">
+      <AsteroidResults :asteroidData="asteroidData" />
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import AsteroidSearchForm from "./AsteroidSearchForm.vue";
+import AsteroidResults from "./AsteroidResults.vue";
 import { getAsteroids } from "../services/Neows.service";
 export default {
   name: "Home",
   components: {
     AsteroidSearchForm,
+    AsteroidResults,
   },
-  data: () => ({}),
+  data: () => ({
+    asteroidData: null,
+  }),
   methods: {
     search: function (searchEvent) {
-      console.log(searchEvent);
       const { fromDate, toDate } = searchEvent;
       // pass in fromDate and toDate to api call
-      getAsteroids(fromDate, toDate).then((res) => console.log(res));
+      getAsteroids(fromDate, toDate).then((res) => (this.asteroidData = res));
     },
   },
 };
