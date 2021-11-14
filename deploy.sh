af9e445c44b51f20bf4513f8ebd86e77e45f3baa
jasonfritsche <fritsche.jason@gmail.com>
2021-11-13 18:42:52 -0600

    updated gh branch in deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jasonfritsche/near-earth-asteroids.git master:gh-pages
+git push -f git@github.com:jasonfritsche/near-earth-asteroids.git main:gh-pages
 cd -
